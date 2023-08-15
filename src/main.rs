619c1a00ea23bfe17c1a6ecb785913c608ccca03
aalhendi <abdulrazaq.alhendi@gmail.com>
2023-08-15 20:00:10 +0300

    feat: handle multiple clients

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,28 @@ use tokio::{
 async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("localhost:8080").await?;
 
-    let (mut socket, _addr) = listener.accept().await?;
-    let (reader, mut writer) = socket.split();
-
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
-
-    // After one client connects, start an echo server.
     loop {
-        let bytes_read = reader.read_line(&mut line).await?;
-        if bytes_read == 0 {
-            break Ok(());
-        }
+        let (mut socket, _addr) = listener.accept().await?;
 
-        // Writes every single byte from input buffer to ouput buffer
-        writer.write_all(line.as_bytes()).await?;
-        line.clear();
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            // After one client connects, start an echo server.
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    std::process::exit(0);
+                }
+
+                // Writes every single byte from input buffer to ouput buffer
+                writer
+                    .write_all(line.as_bytes())
+                    .await
+                    .expect("Could not write line.");
+                line.clear();
+            }
+        });
     }
 }
